232c9685fe5246da9c43cb7c9b82b18f6aa39818
pdtx <2461812105@qq.com>
2018-12-18 23:03:38 +0800

    add

---
+++ concurrent/src/main/java/chapter4/MultiThread.java
@@ -0,0 +1,20 @@
+/**
+ * @description:
+ * @author: fancying
+ * @create: 2018-12-18 22:16
+ **/
+package chapter4;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+public class MultiThread {
+    public static void main(String[] args) {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false,false);
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "]" + threadInfo.getThreadName());
+        }
+    }
+}
