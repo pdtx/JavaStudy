e26e098529e16a0daa98891c7c811fc57cdb064f
pdtx <2461812105@qq.com>
2018-12-26 20:34:31 +0800

    add

---
+++ concurrent/src/main/java/chapter10/FutureTest.java
@@ -0,0 +1,13 @@
+/**
+ * @description: Future demo
+ * @author: fancying
+ * @create: 2018-12-26 20:07
+ **/
+package chapter10;
+
+
+/**
+ *  当一个线程需要等待另一个线程把某个任务执行完后它才能继续执行，此时可以使用FutureTask
+ * */
+public class FutureTest {
+}
