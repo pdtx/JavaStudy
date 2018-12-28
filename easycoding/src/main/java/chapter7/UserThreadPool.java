2f9b512b7434abc82c856d8fe012a901c03355c2
pdtx <2461812105@qq.com>
2018-12-28 23:03:34 +0800

    add

---
+++ easycoding/src/main/java/chapter7/UserThreadPool.java
@@ -0,0 +1,19 @@
+/**
+ * @description: Executors 线程工厂 拒绝策略示例
+ * @author: fancying
+ * @create: 2018-12-28 22:45
+ **/
+package chapter7;
+
+public class UserThreadPool {
+
+    public static void main(String[] args) {
+        int c = Integer.SIZE -3 ;
+
+        int m = (1 << c)  ;
+        int n = m -1;
+        System.out.println(m);
+        System.out.println(Integer.toBinaryString(m));
+        System.out.println(Integer.toBinaryString(n));
+    }
+}
