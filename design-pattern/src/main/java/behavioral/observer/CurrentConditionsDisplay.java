abf337463bea421c3d23007518cf929904ecd77c
pdtx <2461812105@qq.com>
2019-01-15 22:45:43 +0800

    add

---
+++ design-pattern/src/main/java/behavioral/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,18 @@
+/**
+ * @description: 布告板
+ * @author: fancying
+ * @create: 2019-01-15 22:41
+ **/
+package behavioral.observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    @Override
+    public void display() {
+        
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
--- design-pattern/src/main/java/behavioral/observer/WeatherData.java
+++ design-pattern/src/main/java/behavioral/observer/WeatherData.java
@@ -5,15 +5,29 @@
  **/
 package behavioral.observer;
 
+import java.util.ArrayList;
+
 public class WeatherData implements Subject {
+    // 记录观察者
+    private ArrayList<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<>();
+    }
     @Override
     public void registerObserver(Observer o) {
-        
+        observers.add(o);
     }
 
     @Override
     public void removeObserver(Observer o) {
-
+        int i = observers.indexOf(o);
+        if (i >= 0) {
+            observers.remove(o);
+        }
     }
 
     @Override
